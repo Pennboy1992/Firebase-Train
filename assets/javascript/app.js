a5c59f403238bbc25fd86c7e909430413637d4be
Mad Marine <pnnroman92@gmail.com>
2019-06-11 20:30:55 -0500

    added html for adding a new train to schedule

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA173r7N_AGTj4qCzfa7sS9VPh8-dblrzY",
+    authDomain: "roman-s-project.firebaseapp.com",
+    databaseURL: "https://roman-s-project.firebaseio.com",
+    projectId: "roman-s-project",
+    storageBucket: "roman-s-project.appspot.com",
+    messagingSenderId: "415611312010",
+    appId: "1:415611312010:web:3003d9d2d3cbaefb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+$(document).ready(function {
+
+
+})
